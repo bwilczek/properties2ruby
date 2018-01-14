847c93e2536bdb0ffa6c0a9da909041d388585e6
Bartek Wilczek <bwilczek@gmail.com>
2018-01-14 09:54:11 +0100

    WIP, added processing of hashes

--- lib/properties2ruby.rb
+++ lib/properties2ruby.rb
@@ -1,4 +1,5 @@
 require 'java-properties'
+require 'active_support/core_ext/hash/deep_merge'
 require_relative 'properties2ruby/config'
 
 module Properties2Ruby
@@ -7,7 +8,7 @@ module Properties2Ruby
       ret = {}
       raw_hash = JavaProperties.parse(string)
       raw_hash.each_pair do |key, value|
-        ret[key] = parse_scalar(value)
+        ret.deep_merge! process_line(key, value)
       end
       ret
     end
@@ -18,12 +19,23 @@ module Properties2Ruby
 
     private
 
-    def parse_scalar(string)
+    def process_line(key, value)
+      # a.b.c=123 => { a: { b: { c: 123 } } }
+      # a.0=123 => a: [123]
+      head, tail = key.to_s.split('.', 2)
+      if tail.nil?
+        { key.to_sym => parse_value_string(value) }
+      else
+        { head.to_sym => process_line(tail, value) }
+      end
+    end
+
+    def parse_value_string(string)
       return true if string == 'true'
       return false if string == 'false'
       # rubocop:disable Style/RescueModifier
-      return Float(string.to_s) rescue nil
       return Integer(string.to_s) rescue nil
+      return Float(string.to_s) rescue nil
       # rubocop:enable Style/RescueModifier
       string
     end
--- spec/properties2ruby_spec.rb
+++ spec/properties2ruby_spec.rb
@@ -19,5 +19,41 @@ RSpec.describe Properties2Ruby do
         expect(Properties2Ruby.parse('key=true')).to eq(key: true)
       end
     end
+
+    describe 'an array' do
+      it 'flat single line' do
+        expect(Properties2Ruby.parse('key.0=123')).to eq(key: [123])
+      end
+
+      it 'nested single line' do
+        expect(Properties2Ruby.parse('key.0.0=123')).to eq(key: [[123]])
+      end
+
+      it 'flat multi line' do
+        expect(Properties2Ruby.parse("a.0=123\na.1=456")).to eq(a: [123, 456])
+      end
+
+      it 'nested multi line' do
+        expect(Properties2Ruby.parse("a.0.0=123\na.0.1=456")).to eq(a: [[123, 456]])
+      end
+    end
+
+    describe 'a hash' do
+      it 'flat single line' do
+        expect(Properties2Ruby.parse('a.b=123')).to eq(a: { b: 123 })
+      end
+
+      it 'nested single line' do
+        expect(Properties2Ruby.parse('a.b.c=123')).to eq(a: { b: { c: 123 } })
+      end
+
+      it 'flat multli line' do
+        expect(Properties2Ruby.parse("a.b=123\na.c=456")).to eq(a: { b: 123, c: 456 })
+      end
+
+      it 'nested multli line' do
+        expect(Properties2Ruby.parse("a.b.c=123\na.b.d=456")).to eq(a: { b: { c: 123, d: 456 } })
+      end
+    end
   end
 end
